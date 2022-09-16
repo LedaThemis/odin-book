1e0835d696932f308d9a192d33a645c97e9887ae
Leda <ledathemis@protonmail.com>
2022-09-16 16:40:53 +0000

    fix: prevent connecting before authentication

--- src/context/SocketProvider.tsx
+++ src/context/SocketProvider.tsx
@@ -1,10 +1,35 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SERVER_ENDPOINT);
+import { useUser } from './UserProvider';
+
+const socket = io(process.env.REACT_APP_SERVER_ENDPOINT, {
+    autoConnect: false,
+    withCredentials: true,
+});
 
 const SocketContext = createContext(socket);
 
+interface ISocketProvider {
+    children: React.ReactNode;
+}
+
+const SocketProvider = ({ children }: ISocketProvider) => {
+    const user = useUser();
+
+    useEffect(() => {
+        if (user && !socket.connected) {
+            socket.connect();
+        }
+    }, [user]);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
 const useSocket = () => useContext(SocketContext);
 
-export { SocketContext, useSocket };
+export { SocketProvider, useSocket };
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,20 +1,12 @@
-import { io } from 'socket.io-client';
-
 import { AuthProvider } from './AuthProvider';
-import { SocketContext } from './SocketProvider';
+import { SocketProvider } from './SocketProvider';
 import { UserProvider } from './UserProvider';
-
-const socket = io(process.env.REACT_APP_SERVER_ENDPOINT, {
-    withCredentials: true,
-});
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
     return (
         <AuthProvider>
             <UserProvider>
-                <SocketContext.Provider value={socket}>
-                    {children}
-                </SocketContext.Provider>
+                <SocketProvider>{children}</SocketProvider>
             </UserProvider>
         </AuthProvider>
     );
