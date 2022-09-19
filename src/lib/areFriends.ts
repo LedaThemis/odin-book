aab0a25777ce3797e0fee558f20d46e5793dacc5
Leda <ledathemis@protonmail.com>
2022-09-19 08:19:13 +0000

    fix: update areFriends to accept AnyUser

--- src/lib/areFriends.ts
+++ src/lib/areFriends.ts
@@ -1,6 +1,7 @@
-import { IAnyUser, IPopulatedUser } from './interfaces/User';
+import { IAnyUser } from './interfaces/User';
 
-const areFriends = (user1: IPopulatedUser, user2: IAnyUser) =>
-    user1.friends.some((u) => u._id === user2._id);
-
+const areFriends = (user1: IAnyUser, user2: IAnyUser) =>
+    user1.friends.some((x) =>
+        typeof x === 'string' ? x === user2._id : x._id === user2._id,
+    );
 export default areFriends;
