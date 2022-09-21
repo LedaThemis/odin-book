b0bbdc87440557d8a2965de87f61ae822982a90d
Leda <ledathemis@protonmail.com>
2022-09-21 22:39:37 +0000

    feat(tests): add tests for unfriendUser

---
+++ src/lib/__tests__/unfriendUser.test.ts
@@ -0,0 +1,21 @@
+import axios from '../axiosInstance';
+import unfriendUser from '../unfriendUser';
+
+jest.mock('../axiosInstance', () => ({
+    delete: (route: string, data: unknown) => ({
+        data: {
+            user: data,
+        },
+    }),
+}));
+
+test('should call axios post with correct arguments', async () => {
+    const userId = '1';
+
+    const spy = jest.spyOn(axios, 'delete');
+
+    await unfriendUser({ userId });
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(`users/${userId}/friend`);
+});
