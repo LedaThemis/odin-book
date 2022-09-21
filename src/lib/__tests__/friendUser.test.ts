4e55b733607084c29d3d1f996d4332e0d4d48837
Leda <ledathemis@protonmail.com>
2022-09-21 19:00:19 +0000

    feat(tests): add tests for friendUser

---
+++ src/lib/__tests__/friendUser.test.ts
@@ -0,0 +1,21 @@
+import axios from '../axiosInstance';
+import friendUser from '../friendUser';
+
+jest.mock('../axiosInstance', () => ({
+    post: (route: string, data: unknown) => ({
+        data: {
+            user: data,
+        },
+    }),
+}));
+
+test('should call axios delete with correct arguments', async () => {
+    const userId = '1';
+
+    const spy = jest.spyOn(axios, 'post');
+
+    await friendUser({ userId });
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(`users/${userId}/friend`);
+});
